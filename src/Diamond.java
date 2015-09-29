14e8b7c81fff987fb6479b2b23326a7fddda76cf
Gregory Estrade <gregory.estrade@gmail.com>
2015-09-29 02:24:52 +0200

    Optimized

--- src/Diamond.java
+++ src/Diamond.java
@@ -1,33 +1,45 @@
 public class Diamond {
 
-    private String spaces(int n) {
-        String res = "";
+    private static StringBuilder spaces(StringBuilder sb, int n) {
         while(n-- > 0)
-            res += "-";
-        return res;
+            sb.append('-');
+        return sb;
     }
 
-    public String build(int c, int level, int dir) {
-        String me = spaces(level) + (char)c + spaces(2*(c - 'A')-1) + (char)c + spaces(level) + "\n";
+    public static void build(StringBuilder sb, int c, int level, int dir) {
         if (c == 'A') {
-            return spaces(level) + 'A' + spaces(level) + "\n";
+            spaces(sb, level);
+            sb.append('A');
+            spaces(sb, level);
+            sb.append('\n');
         } else {
-            if (dir == 0) {
-                return build(c - 1, level + 1, -1) + me + build(c - 1, level + 1, 1);
-            } else if (dir > 0) {
-                return me + build(c - 1, level + 1, dir);
-            } else {
-                return build(c - 1, level + 1, dir) + me;
+            if (dir <= 0) {
+                build(sb, c - 1, level + 1, -1);
+            }
+            spaces(sb, level);
+            sb.append((char)c);
+            spaces(sb, 2*(c - 'A')-1);
+            sb.append((char)c);
+            spaces(sb, level);
+            sb.append('\n');
+            if (dir >= 0) {
+                build(sb, c - 1, level + 1, 1);
             }
         }
     }
 
-    public void draw(char root) {
-        System.out.print(build(root, 0, 0));
+    public static String create(char root) {
+        StringBuilder sb = new StringBuilder();
+        build(sb, root, 0, 0);
+        return sb.toString();
     }
 
     public static void main(String args[]) {
-        Diamond d = new Diamond();
-        d.draw('J');
+        System.out.println(Diamond.create('F'));
+        long now = System.currentTimeMillis();
+        for(int i = 0; i < 1000000; i++) {
+            Diamond.create('Z');
+        }
+        System.out.println(System.currentTimeMillis() - now);
     }
 }
---
+++ src/fr/craftinglabs/kata/DiamondAV.java
@@ -0,0 +1,73 @@
+package fr.craftinglabs.kata;
+
+public class DiamondAV {
+
+    private static final String SEPARATOR = "-";
+    private static final String END_OF_LINE = "\n";
+
+    public static String create(Character c) {
+        int size = sizeOfDIamond(c);
+
+        if(size == 1) return "A";
+
+        return buildDiamond(size);
+    }
+
+    private static String buildDiamond(int size) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(diamondTip(size)).append(END_OF_LINE);
+
+        for (int floor = size - 2; floor >= -(size - 2) ; floor --) {
+            builder.append(diamondWall(size, floor)).append(END_OF_LINE);
+        }
+
+        builder.append(diamondTip(size));
+
+        return builder.toString();
+    }
+
+    private static int sizeOfDIamond(Character c) {
+        return c - 'A' + 1;
+    }
+
+    private static String diamondWall(int size, int floor) {
+        int width = size * 2 - 1;
+        int absoluteFloor = Math.abs(floor);
+
+        Character wall = wallForTheFloor(size, absoluteFloor);
+        StringBuilder builder = new StringBuilder();
+
+        return builder
+                .append(manySpaces(absoluteFloor))
+                .append(wall)
+                .append(manySpaces(width - 2*(absoluteFloor + 1)))
+                .append(wall)
+                .append(manySpaces(absoluteFloor))
+                .toString();
+    }
+
+    private static char wallForTheFloor(int size, int absoluteFloor) {
+        return Character.toChars('A' + size - absoluteFloor -1)[0];
+    }
+
+    private static String diamondTip(int size) {
+        return manySpaces(size -1) + "A" + manySpaces(size - 1);
+    }
+
+    private static String manySpaces(int nbSpaces) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < nbSpaces; i++) {
+            builder.append(SEPARATOR);
+        }
+        return builder.toString();
+    }
+
+    public static void main(String args[]) {
+        long now = System.currentTimeMillis();
+        System.out.println(DiamondAV.create('F'));
+        for(int i = 0; i < 1000000; i++)
+            DiamondAV.create('Z');
+        System.out.println(System.currentTimeMillis() - now);
+    }
+}
